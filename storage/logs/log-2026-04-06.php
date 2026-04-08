5f1bce8fd011a6cf5e7e8c4f8944f1a5174ebdfc
Guswana <222112082@stis.ac.id>
2026-04-08 15:05:05 +0700

    Initial commit

---
+++ storage/logs/log-2026-04-06.php
@@ -0,0 +1,131 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-06 05:06:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:06:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:06:36 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:06:36 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:26 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:26 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:27 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:28 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:28 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:31 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:38 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:38 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:41 --> Unable to connect to the database
+ERROR - 2026-04-06 05:08:42 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:42 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:42 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:43 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 05:08:43 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:21 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:21 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:22 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:22 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:26 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:26 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:27 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:29 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:30 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:30 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:33 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:33 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:33 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:49 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:50 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:52 --> Unable to connect to the database
+ERROR - 2026-04-06 08:46:53 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:53 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:53 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:54 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 08:46:54 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:23:45 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:23:45 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:23:46 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:10 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:11 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:11 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:12 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:13 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:13 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:19 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:19 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:22 --> Unable to connect to the database
+ERROR - 2026-04-06 09:25:22 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:22 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:25:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:30 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:30 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:30 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:32 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:33 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:34 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:34 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:35 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:36 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:36 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:37 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:44 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:44 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:47 --> Unable to connect to the database
+ERROR - 2026-04-06 09:29:47 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:48 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:48 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:48 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 09:29:49 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:37:56 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:37:57 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:00 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:00 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:03 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:05 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:05 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:10 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:10 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:11 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:12 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:13 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:13 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:19 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:20 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:23 --> Unable to connect to the database
+ERROR - 2026-04-06 10:38:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:23 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:24 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:24 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 10:38:24 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 11:07:47 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 11:07:48 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 11:07:49 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+ERROR - 2026-04-06 11:07:49 --> Session: Configured save path 'C:\Windows\Temp' is not writable by the PHP process.
+NOTICE - 2026-04-06 11:12:57 --> Mulai memasang data awal
+NOTICE - 2026-04-06 11:13:55 --> Berhasil memperbarui data awal tabel setting_aplikasi
+NOTICE - 2026-04-06 11:13:55 --> Berhasil memperbarui data awal tabel setting_modul
+NOTICE - 2026-04-06 11:13:55 --> Berhasil memperbarui data awal tabel user_grup
+NOTICE - 2026-04-06 11:13:55 --> Berhasil memperbarui data awal tabel user
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel media_sosial
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel kehadiran_jam_kerja
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel ref_jabatan
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel anjungan_menu
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel gis_simbol
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel ref_syarat_surat
+NOTICE - 2026-04-06 11:13:56 --> Berhasil memperbarui data awal tabel widget
+NOTICE - 2026-04-06 11:13:57 --> Berhasil memperbarui data awal tabel tweb_penduduk_umur
+NOTICE - 2026-04-06 11:14:01 --> Berhasil memperbarui data awal tabel notifikasi
+NOTICE - 2026-04-06 11:14:15 --> Berhasil Jalankan Migrasi_2024010171
+NOTICE - 2026-04-06 11:14:18 --> Berhasil Jalankan Migrasi_2024020171
+NOTICE - 2026-04-06 11:14:18 --> Berhasil Jalankan Migrasi_2024030171
+NOTICE - 2026-04-06 11:14:18 --> Berhasil Jalankan Migrasi_2024031351
+NOTICE - 2026-04-06 11:14:19 --> Berhasil Jalankan Migrasi_2024032051
+NOTICE - 2026-04-06 11:14:19 --> Berhasil Jalankan Migrasi_2024091201
+NOTICE - 2026-04-06 11:14:19 --> Berhasil Jalankan Migrasi_2024100101
+NOTICE - 2026-04-06 11:14:19 --> Berhasil Jalankan migrasi_dev
+NOTICE - 2026-04-06 11:14:22 --> Versi database sudah terbaru
+ERROR - 2026-04-06 11:14:22 --> updateConfigFile password - eyJpdiI6ImNZSjlnQkJVc3lBdVZ1UzBLM1ZySEE9PSIsInZhbHVlIjoidEh0SzR1UDVPWEdKWHNlYkVoUk9WQT09IiwibWFjIjoiMGIzNGVhYzhhYjMwMTFlMTM2NWYzZTQzNTc1MDdkOTQxMGNjZGRlNzkzMmU5ODhhOTAzMTEyOTU2MGExNTEwZSIsInRhZyI6IiJ9
+NOTICE - 2026-04-06 11:14:22 --> Selesai memasang data awal
