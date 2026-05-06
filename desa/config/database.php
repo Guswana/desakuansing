a4769b3051730fd6497e11dfb51485c617bc9e4f
Guswana <222112082@stis.ac.id>
2026-05-06 19:14:50 +0700

    save

--- desa/config/database.php
+++ desa/config/database.php
@@ -9,9 +9,10 @@
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'eyJpdiI6ImNZSjlnQkJVc3lBdVZ1UzBLM1ZySEE9PSIsInZhbHVlIjoidEh0SzR1UDVPWEdKWHNlYkVoUk9WQT09IiwibWFjIjoiMGIzNGVhYzhhYjMwMTFlMTM2NWYzZTQzNTc1MDdkOTQxMGNjZGRlNzkzMmU5ODhhOTAzMTEyOTU2MGExNTEwZSIsInRhZyI6IiJ9';
+$db['default']['password'] = 'eyJpdiI6InBOaHFIU1JmT2hlMEdqRmdWbzc2NEE9PSIsInZhbHVlIjoiaVI4LytSZm4ydmhVcFRhc3YyRG1UZz09IiwibWFjIjoiMDg2N2E4ZTQ2MzRlOTc4ZmU3MzZlZTY5YmIxZWMxYzI3NzQ4MzkzNDU0YjEwOTUyMjI5NTZlMzg2NTI5ZTdmMiIsInRhZyI6IiJ9';
 $db['default']['port']     = 3306;
-$db['default']['database'] = 'desakuansing';
+$db['default']['database'] = 'perhentianluas_sicantik';
+// $db['default']['database'] = 'desakuansing';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 
 /*
